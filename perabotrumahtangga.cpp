0b9be2e9a4a1062d5e1f685838720d5382c58783
dineart <di2601na@gmail.com>
2025-06-23 10:41:18 +0800

    Struktur dasar program inventory circular liked list

--- perabotrumahtangga.cpp
+++ perabotrumahtangga.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 #include <string>
-using namepsace std;
+using namespace std;
+
+struct Perabot {
+    int id;
+    string nama;
+    string kategori;
+    string merk;
+    double harga;
+    int stok;
+    Perabot* next;
+
+};
 
