fe127851255ee1c33d5172e47d66f77d223dccf7
dineart <di2601na@gmail.com>
2025-06-23 10:53:49 +0800

    Menu interaktif untuk pilihan pengguna

--- perabotrumahtangga.cpp
+++ perabotrumahtangga.cpp
@@ -13,3 +13,43 @@ struct Perabot {
 
 };
 
+
+
+int main () {
+    int pilihan, posisi, idHapus;
+
+    do{
+        cout << "\n=== Inventory Perabotan Rumah Tangga (Circular Linked List) ===\n";
+        cout << "1. Tambah Perabot\n";
+        cout << "2. Sisipkan Perabotan di Posisi\n";
+        cout << "3. Tampilkan Semua Perabotan\n";
+        cout << "4. Hapus Perabotan berdasarkan ID\n";
+        cout << "0. Keluar\n";
+        cout << "Pilihan Anda : ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                tambahPerabot();
+                break;
+            case 2:
+                cout << "Masukkan Posisi: ";
+                cin >> posisi;
+                sisipkanPerabot(posisi);
+                break;
+            case 3:
+                tampilkanPerabot();
+                break;
+            case 4:
+                cout << "Masukkan ID perabot yang ingin dihapus: ";
+                cin >> idHapus;
+                hapusPerabot(idHapus);
+                break;
+            case 0:
+                cout << "Program selesai.\n";
+                break;
+            default:
+                cout << "Pilihan tidak valid.\n";    
+        }
+    }
+}
